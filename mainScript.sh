6122c39884d53814a28e8285d32f0595e730584e
shravan-kaja <shravan.kaja@gmail.com>
2021-01-19 17:32:31 +0530

    Added: Computation Processing

--- mainScript.sh
+++ mainScript.sh
@@ -7,3 +7,11 @@ read secondNumber
 echo "Enter Third Number"
 read thirdNumber
 
+resultFirst=$(( $firstNumber + $secondNumber * $thirdNumber ))
+resultSecond=$(( $firstNumber * $secondNumber + $thirdNumber ))
+resultThird=$(( $thirdNumber + $firstNumber / $secondNumber ))
+resultFourth=$(( $firstNumber % $secondNumber * $thirdNumber ))
+echo $resultFirst
+echo $resultSecond
+echo $resultThird
+echo $resultFourth
