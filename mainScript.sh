af8b987753a88bfd45908b8842ee8b7b4f672c65
shravan-kaja <shravan.kaja@gmail.com>
2021-01-19 17:22:07 +0530

    Added: Input Processing

--- mainScript.sh
+++ mainScript.sh
@@ -1,1 +1,9 @@
 echo "Computation and  Sorting"
+echo "Enter Number "
+echo "Enter First Number"
+read firstNumber
+echo "Enter Second Number"
+read secondNumber
+echo "Enter Third Number"
+read thirdNumber
+
