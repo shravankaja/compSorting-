0c4925e317ffe0034a99356a1c6ce4e42b7491e2
shravan-kaja <shravan.kaja@gmail.com>
2021-01-19 17:55:51 +0530

    Refactor: Computation Processing

--- mainScript.sh
+++ mainScript.sh
@@ -1,4 +1,4 @@
-echo "Computation and  Sorting"
+\echo "Computation and  Sorting"
 echo "Enter Number "
 echo "Enter First Number"
 read firstNumber
@@ -6,12 +6,44 @@ echo "Enter Second Number"
 read secondNumber
 echo "Enter Third Number"
 read thirdNumber
+c1=1
+c2=2
+c3=3
+c4=4
+echo " 1) A+B*C 2) A*B+C 3) A+B/C 4) A%B*C":
+read choice
+case $choice in 
+	$c1) 
+	resultFirst=$(( $firstNumber + $secondNumber * $thirdNumber ))
+	echo $resultFirst ;;
+	
+	
+	$c2)
+	resultSecond=$(( $firstNumber * $secondNumber + $thirdNumber )) 
+	echo $resultSecond ;;
 
-resultFirst=$(( $firstNumber + $secondNumber * $thirdNumber ))
-resultSecond=$(( $firstNumber * $secondNumber + $thirdNumber ))
-resultThird=$(( $thirdNumber + $firstNumber / $secondNumber ))
-resultFourth=$(( $firstNumber % $secondNumber * $thirdNumber ))
-echo $resultFirst
-echo $resultSecond
-echo $resultThird
-echo $resultFourth
+	$c3)
+	resultThird=$(( $thirdNumber + $firstNumber / $secondNumber )) 
+	echo $resultThird ;;
+
+	$c4)
+	resultFourth=$(( $firstNumber % $secondNumber * $thirdNumber )) 
+	echo $resultFourth ;;
+
+esac
+
+	resultFirst=$(( $firstNumber + $secondNumber * $thirdNumber ))
+    	resultSecond=$(( $firstNumber * $secondNumber + $thirdNumber ))
+        resultThird=$(( $thirdNumber + $firstNumber / $secondNumber ))
+        resultFourth=$(( $firstNumber % $secondNumber * $thirdNumber ))
+
+arr=( $resultFirst $resultSecond $resultThird $resultFourth )
+ 
+for (( i=0 ; $i < 4 ; i++ ))
+do 
+	arr["result"$i]=${arr[i]}
+done
+for i1 in "${arr[@]}"
+do
+	echo $i1
+done
